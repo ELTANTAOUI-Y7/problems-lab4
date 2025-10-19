25b73931bf54445bd6c173154f1dcd1a596230a6
Yahya EL-TANTAOUI <Yahya.EL-TANTAOUI@um6p.ma>
2025-10-19 15:43:21 +0100

    done

---
+++ challenge/src/main/java/problem6/Forme.java
@@ -0,0 +1,7 @@
+package problem6;
+
+public interface Forme {
+    public double getSurface();
+    @Override
+    public String toString();
+}
---
+++ challenge/src/main/java/problem6/Square.java
@@ -0,0 +1,16 @@
+package problem6;
+
+public class Square implements Forme {
+    private double side;
+    public Square(double side){
+        this.side = side;
+    }
+    @Override
+    public double getSurface(){
+        return side * side;
+    }
+    @Override
+    public String toString(){
+        return "Square (side " + side +" cm)";
+    }
+}
